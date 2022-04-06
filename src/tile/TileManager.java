be79323233abcccad1ef388611dc6832ab96536f
Martin Parunev <parunev@gmail.com>
2022-04-06 19:43:06 +0300

    Adding a few more tiles.

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -35,6 +35,15 @@ public class TileManager {
             tile[2] = new Tile();
             tile[2].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/res/water.png")));
 
+            tile[3] = new Tile();
+            tile[3].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/res/earth.png")));
+
+            tile[4] = new Tile();
+            tile[4].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/res/tree.png")));
+
+            tile[5] = new Tile();
+            tile[5].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/res/sand.png")));
+
         }catch(IOException e){
             e.printStackTrace();
         }
