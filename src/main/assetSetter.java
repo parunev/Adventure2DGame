b336c77eeb5f26e9c7ab1b828e97f378a5a18943
Martin Parunev <parunev@gmail.com>
2022-04-08 18:00:11 +0300

    Adding new world map, new tiles and rearranging objects positions

--- src/main/assetSetter.java
+++ src/main/assetSetter.java
@@ -29,7 +29,7 @@ public class assetSetter {
 
         gp.obj[3] = new objectDoor(gp);
         gp.obj[3].worldX = 10 * gp.tileSize;
-        gp.obj[3].worldY = 11 * gp.tileSize;
+        gp.obj[3].worldY = 12 * gp.tileSize;
 
         gp.obj[4] = new objectDoor(gp);
         gp.obj[4].worldX = 8 * gp.tileSize;
@@ -37,11 +37,11 @@ public class assetSetter {
 
         gp.obj[5] = new objectDoor(gp);
         gp.obj[5].worldX = 12 * gp.tileSize;
-        gp.obj[5].worldY = 22 * gp.tileSize;
+        gp.obj[5].worldY = 23 * gp.tileSize;
 
         gp.obj[6] = new objectChest(gp);
         gp.obj[6].worldX = 10 * gp.tileSize;
-        gp.obj[6].worldY = 7 * gp.tileSize;
+        gp.obj[6].worldY = 8 * gp.tileSize;
 
         gp.obj[7] = new objectBoots(gp);
         gp.obj[7].worldX = 37 * gp.tileSize;
